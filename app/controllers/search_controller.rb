b6bfb551c19e83f32cbddbacde5e177f04b74efb
Allen Chen <allen.sl.chen@gmail.com>
2010-02-12 21:24:32 +0000

    search method! yay!

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,39 @@
 class SearchController < ApplicationController
 	def context
-		@query = params[:word];
-		@contexts = []
+		@query = params[:word]
+		@contexts = Array.new()
+		fa = Array.new()
+		fa << "h.txt"
+		fa << "s.txt"
+
+		for filename in fa do
+			f = File.open(filename)
+			buf = Array.new(3)
+			buf[0] = f.gets
+			buf[1] = f.gets
+			buf[2] = f.gets
+			#puts buf
+			count = 0;
+			while (line = f.gets)
+				if (line != "\n")
+					if (buf[1].include?(@query))
+						#puts buf
+						#puts @contexts
+						#puts "FUN HAPPENS HERE"
+						@contexts << Array.new(buf)
+						puts @contexts
+						#puts buf
+						count = count+1
+					end
+					buf.shift
+					buf << line
+				end
+			end
+		end
+		puts @contexts
 	end
 	
 	def search
+		
 	end
 end
