1e3dda537ee15b26813b7acbe2268a8a3543dad1
Allen Chen <allen.sl.chen@gmail.com>
2010-02-19 20:49:10 +0000

    wow ruby i hate you

---
+++ db/migrate/20100219204600_fix_wordlists_words_join_table2.rb
@@ -0,0 +1,17 @@
+class FixWordlistsWordsJoinTable2 < ActiveRecord::Migration
+  def self.up
+    create_table :wordlists_words, {:id => false} do |t|
+	   t.integer :word_id
+	   t.integer :wordlist_id
+	 end
+	drop_table :wordlists_words
+  end
+
+  def self.down
+     drop_table :wordlists_words
+	 create_table :wordlists_words do |t|
+	   t.integer :word_id
+	   t.integer :wordlist_id
+	 end
+  end
+end
