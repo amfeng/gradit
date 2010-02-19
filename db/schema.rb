60fb5e6d164c829a71d1c51f50c3b5bce6b718fc
Amber Feng <amber.feng@gmail.com>
2010-02-19 20:32:17 +0000

    cool new words!

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100219192914) do
+ActiveRecord::Schema.define(:version => 20100219202946) do
 
   create_table "book_lines", :force => true do |t|
     t.text     "line"
