a3caf3a59b4a9897006557edc717b89307aebdb5
Allen Chen <allen.sl.chen@gmail.com>
2010-04-11 05:46:04 +0000

    Fixed migration issue (dropping a table that doesn't exist!)

--- db/migrate/20100219204600_fix_wordlists_words_join_table2.rb
+++ db/migrate/20100219204600_fix_wordlists_words_join_table2.rb
@@ -1,6 +1,5 @@
 class FixWordlistsWordsJoinTable2 < ActiveRecord::Migration
   def self.up
-    drop_table :wordlists_words
     create_table :wordlists_words, {:id => false} do |t|
 	   t.integer :word_id
 	   t.integer :wordlist_id
@@ -9,9 +8,5 @@ class FixWordlistsWordsJoinTable2 < ActiveRecord::Migration
 
   def self.down
      drop_table :wordlists_words
-	 create_table :wordlists_words do |t|
-	   t.integer :word_id
-	   t.integer :wordlist_id
-	 end
   end
 end
