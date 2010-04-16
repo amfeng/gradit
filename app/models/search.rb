76b7888d43c367934081d6bdfdec2a0d6cf0cfda
Allen Chen <allen.sl.chen@gmail.com>
2010-04-16 20:14:52 +0000

    fixed armando's eurosys problem

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,14 +4,18 @@ class Search < ActiveRecord::Base
 		@contexts = Array.new()
 		la = BookLine.find(:all, :conditions => ["line like ?", "%" + query + "%"])
 
+		reg = /\b#{query}\b/i
+		
 		for line in la do
-			beforeline = BookLine.find(:first, :conditions => ["linenum = ? and source = ?", line.linenum - 1, line.source])
-			afterline = BookLine.find(:first, :conditions => ["linenum = ? and source = ?", line.linenum + 1, line.source])
-			if (beforeline != nil) then beforeline = beforeline.line end
-			if (afterline != nil) then afterline = afterline.line end
-			bookname = Book.find(line.source).name
-			if (line != nil) then linecontent = line.line end
-			@contexts << [beforeline, linecontent, afterline, bookname]
+			if (reg.match(line)) do
+				beforeline = BookLine.find(:first, :conditions => ["linenum = ? and source = ?", line.linenum - 1, line.source])
+				afterline = BookLine.find(:first, :conditions => ["linenum = ? and source = ?", line.linenum + 1, line.source])
+				if (beforeline != nil) then beforeline = beforeline.line end
+				if (afterline != nil) then afterline = afterline.line end
+				bookname = Book.find(line.source).name
+				if (line != nil) then linecontent = line.line end
+				@contexts << [beforeline, linecontent, afterline, bookname]
+			end
 		end
 		return @contexts
 	end
