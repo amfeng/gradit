ecd62a8b192c12a6ef095010cafb62a361c48dda
Sunil Pedapudi <skpedapudi@gmail.com>
2010-04-30 20:07:16 +0000

    bug fixes in search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,22 +1,23 @@
 class Search < ActiveRecord::Base
-	
-	def self.search(query)
-		@contexts = Array.new()
-		la = BookLine.find(:all, :conditions => ["line like ?", "%" + query + "%"])
-
-		reg = /\b#{query}\b/i
-		
-		for line in la do
-			if (reg.match(line)) do
-				beforeline = BookLine.find(:first, :conditions => ["linenum = ? and source = ?", line.linenum - 1, line.source])
-				afterline = BookLine.find(:first, :conditions => ["linenum = ? and source = ?", line.linenum + 1, line.source])
-				if (beforeline != nil) then beforeline = beforeline.line end
-				if (afterline != nil) then afterline = afterline.line end
-				bookname = Book.find(line.source).name
-				if (line != nil) then linecontent = line.line end
-				@contexts << [beforeline, linecontent, afterline, bookname]
-			end
-		end
-		return @contexts
-	end
+  
+  def self.search(query)
+    @contexts = Array.new()
+    la = BookLine.find(:all, :conditions => ["line like ?", "%" + query + "%"])
+    
+    reg = /\b#{query}\b/i
+    
+    for line in la do
+      if (reg.match(line.line))
+        beforeline = BookLine.find(:first, :conditions => ["linenum = ? and source = ?", line.linenum - 1, line.source])
+        afterline = BookLine.find(:first, :conditions => ["linenum = ? and source = ?", line.linenum + 1, line.source])
+        if (beforeline != nil) then beforeline = beforeline.line end
+        if (afterline != nil) then afterline = afterline.line end
+        bookname = Book.find(line.source).name
+        if (line != nil) then linecontent = line.line end
+        @contexts << [beforeline, linecontent, afterline, bookname]   
+      end
+    end
+    return @contexts
+  end
 end
+
