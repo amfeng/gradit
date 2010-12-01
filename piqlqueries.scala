6b398650cfdd355919bd63d7fe9ebfb01656d341
U-Allens-Laptop\Allen <Allen@Allens-Laptop.(none)>
2010-12-01 13:50:17 -0800

    updated piqlqueries, schema changed (primitives only in pkeys) and changed queries

--- piqlqueries.scala
+++ piqlqueries.scala
@@ -6,35 +6,40 @@ import edu.berkeley.cs.avro.marker._
 
 import org.apache.avro.util._
 
-case class Word(var word: String) extends AvroPair {
+case class Word(var id: Int) extends AvroPair {
   //assign PK int to do randomness, but need to provide int when loading in words
+  var word: String = _
   var definition: String = _
-  var wordlist: WordList = _
+  var wordlist: String = _
+}
+
+case class WordList_Word(var wordlist: String, var word: Int) extends AvroPair {
+    var v = 1
 }
 
 case class WordList(var name: String) extends AvroPair{
-
+    var v = 1
 }
 
 case class Book(var title: String) extends AvroPair {
-
+    var v = 1
 }
 
 
 //call WORDcontext
-case class Context(var word: Word, var book: Book, var linenum: Int) extends AvroPair {
-    // PKEY: var book: Book = _
+case class WordContext(var word: Int, var book: String, var linenum: Int) extends AvroPair {
+    // PKEY: var book: String = _ (book name)
     // PKEY: var linenum: Integer = _
-    // PKEY: var word: Word = _
+    // PKEY: var word: Int = _ (word ID)
     var wordLine: String = _
 }
 /*
 case class Context(var contextId: Int) extends AvroPair {
-  var word: Word = _
-  var book: Book = _
-  var wordLine: String = _
-  var before: String = _
-  var after: String = _
+var word: Word = _
+var book: Book = _
+var wordLine: String = _
+var before: String = _
+var after: String = _
 }
 */
 
@@ -47,8 +52,9 @@ class GraditClient(val cluster: ScadsCluster, executor: QueryExecutor) {
 
   lazy val words = cluster.getNamespace[Word]("words")
   lazy val books = cluster.getNamespace[Book]("books")
-  lazy val contexts  = cluster.getNamespace[Context]("contexts")
-  lazy val wordlists  = cluster.getNamespace[WordList]("wordlists")
+  lazy val wordcontexts = cluster.getNamespace[WordContext]("wordcontexts")
+  lazy val wordlists = cluster.getNamespace[WordList]("wordlists")
+  lazy val wordlist_word = cluster.getNamespace[WordList_Word]("wordlist_word")
 
   private def exec(plan: QueryPlan, args: Any*) = {
     val iterator = executor(plan, args:_*)
@@ -61,34 +67,31 @@ class GraditClient(val cluster: ScadsCluster, executor: QueryExecutor) {
   //contextsForWord
   // Finds all contexts for a particular word given
   
-    def contextsForWord = (
+    val contextsForWord = (
         words
-            .where("word".a === (0.?))
-            .join(contexts)
-            .where("context.word".a === "word".a) // !Q - reference
+            .where("wordcontext.word".a === (0.?))
             .limit(50)
-            // !Q: random?
-    )
+    ).toPiql
   
   //wordsFromWordlist
-    def wordsFromWordList = (
-        wordlists
-            .where("name".a === (0.?))
-            .join(words)
-            .where("word.wordlist".a === "wordlist".a) // !Q - how do I reference this?
+    val wordsFromWordList = (
+        wordlist_word
+            .where("wordlist_word.wordlist".a === (0.?))
             .limit(50)
-    )
+            .join(words)
+            .where("word.id".a === "wordlist_word.word".a)
+    ).toPiql
   
   /*def findUser = users.where("username".a === (0.?))
 
-  def thoughtstream = (
-    subscriptions
-      .where("owner".a === (0.?))
-      .where("approved".a === true)
-      .join(thoughts)
-      .where("thoughts.owner".a === "subscriptions.target".a)
-      .sort("timestamp" :: Nil, false)
-      .limit(10)
-  )*/
+def thoughtstream = (
+subscriptions
+.where("owner".a === (0.?))
+.where("approved".a === true)
+.join(thoughts)
+.where("thoughts.owner".a === "subscriptions.target".a)
+.sort("timestamp" :: Nil, false)
+.limit(10)
+)*/
 
 }
