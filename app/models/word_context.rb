ab6af2f94f0b5580e70c3d4b9943354a53e6d444
allenchen <Allen@Allen-Laptop.(none)>
2011-01-14 14:01:55 -0800

    ok models actually added

---
+++ app/models/word_context.rb
@@ -0,0 +1,5 @@
+
+class WordContext < AvroRecord
+	#belongs_to :book
+	#belongs_to :word
+end
---
+++ app/models/word_list.rb
@@ -0,0 +1,4 @@
+class WordList < AvroRecord
+   #has_and_belongs_to_many :words
+   #has_many :games
+end
